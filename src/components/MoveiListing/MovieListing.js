4392eab3d01c6fbd506c5455fb22bd2d695073b3
Sergey <levashov04sergey@gmail.com>
2021-11-10 14:34:50 +0200

    styling MovieListing & MovieCard

--- src/components/MoveiListing/MovieListing.js
+++ src/components/MoveiListing/MovieListing.js
@@ -2,16 +2,21 @@ import React from 'react';
 import {useSelector} from "react-redux";
 import {getAllMovies} from "../../features/movies/movieSlice";
 import MovieCard from "../MovieCard/MovieCard";
+import "./MovieListing.scss";
 
 const MovieListing = () => {
     const movies = useSelector(getAllMovies);
     // let renderMovies;
 
     let renderMovies = movies.Response === "True" ? (
-        movies.Search.map((movie, index) => {
-            <MovieCard key={index} data={movie}/>;
-        })
-    ) : (<div className="movies-error"><h3>{movies.Error}</h3></div>)
+        movies.Search.map((movie, index) => (
+            <MovieCard key={index} data={movie}/>
+        ))
+    ) : (
+        <div className="movies-error">
+            <h3>{movies.Error}</h3>
+        </div>
+    );
 
     return (
         <div className="movie-wrapper">
