ec8aac72034d98c316f04688aaa97701b77c191e
Sergey <levashov04sergey@gmail.com>
2021-11-10 13:24:23 +0200

    get data using axios for API call

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import MovieListing from "../MoveiListing/MovieListing";
+import movieApi from "../../common/apis/movieApi";
+import {APIKey} from "../../common/apis/MovieApiKey";
 
 const Home = () => {
+
+    useEffect(() => {
+        const movieText = 'Harry';
+        const fetchMovies = async () => {
+            const response = await movieApi.get(`?apiKey=${APIKey}&s=${movieText}&type=movie`)
+                .catch((err) => {
+                    console.log("Err: ", err);
+                });
+            console.log("The response from API ", response);
+        };
+
+        fetchMovies();
+    }, []);
+
     return (
         <div>
             <div className="banner-img"></div>
