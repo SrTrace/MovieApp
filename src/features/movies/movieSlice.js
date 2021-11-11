76d2c48a60172dd25b27ed01f9dc42d59876b1ac
Sergey <levashov04sergey@gmail.com>
2021-11-11 14:42:06 +0200

    add shows to MovieListing comp and  add action - fetch shows

--- src/features/movies/movieSlice.js
+++ src/features/movies/movieSlice.js
@@ -8,12 +8,28 @@ export const fetchAsyncMovies = createAsyncThunk(
     async () => {
         const movieText = 'Harry';
         const response = await movieApi
-            .get(`?apiKey=${APIKey}&s=${movieText}&type=movie`);
+            .get(
+                `?apiKey=${APIKey}&s=${movieText}&type=movie`
+            );
+        console.log(response.data);
+        return response.data;
+    });
+
+export const fetchAsyncShows = createAsyncThunk(
+    'movies/fetchAsyncShows',
+    async () => {
+        const seriesText = 'Friends';
+        const response = await movieApi
+            .get(
+                `?apiKey=${APIKey}&s=${seriesText}&type=series`
+            );
+        console.log(response.data);
         return response.data;
     });
 
 const initialState = {
     movies: {},
+    shows: {},
 };
 
 const movieSlice = createSlice({
@@ -35,9 +51,14 @@ const movieSlice = createSlice({
         [fetchAsyncMovies.rejected]: () => {
             console.log("Rejected!");
         },
+        [fetchAsyncShows.fulfilled]: (state, {payload}) => {
+            console.log("Fetched Successfully!");
+            return {...state, shows: payload};
+        },
     }
 });
 
 export const {addMovies} = movieSlice.actions;
 export const getAllMovies = (state) => state.movies.movies;
+export const getAllShows = (state) => state.movies.shows;
 export default movieSlice.reducer;
